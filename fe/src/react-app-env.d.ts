a577cd3f05e333389012aa111d91a0eb6cf9c925
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-11-01 12:24:02 +0200

    GHpage created, readme added

---
+++ fe/src/react-app-env.d.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.png' {
+  const value: any;
+
+  export = value;
+}
+
+declare module '*.svg' {
+  const value: any;
+
+  export = value;
+}
+
+declare module '*.scss' {
+  const value: any;
+
+  export = value;
+}
