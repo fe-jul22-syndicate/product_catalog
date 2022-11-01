865cdd804173b1fce382ea967d782aabf02e4b33
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-11-01 14:37:02 +0200

    express with typescrypt added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,9 @@
-// some code
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT != null || 5000;
+
+app.get('/', (req, res) => res.send('Hello from server!'));
+
+app.listen(PORT);
