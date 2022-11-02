c12bd6582b7768807bb9ffa6257d502fbe4587e9
Pavel Ohiiko <timurid346@gmail.com>
2022-11-02 19:53:49 +0200

    added footer

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Footer } from './components/footer/Footer';
 import { Header } from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App page__content">
       <Header />
+      <main className="page__main"></main>
+      <Footer />
     </div>
   );
 }
