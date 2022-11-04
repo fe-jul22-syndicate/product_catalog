64dabf5eddb69c6c52a8b394f483bf32d1318b4b
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-11-04 16:11:02 +0200

    fe api added

---
+++ fe/src/api/apiService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const URL = 'https://syndicate-product-catalog.herokuapp.com';
+
+export const getPhones = async (limit = 10, page = 1) => {
+  const phonesResponse = await axios.get(`${URL}/products`, {
+    params: {
+      limit,
+      page,
+    },
+  });
+
+  return phonesResponse.data;
+};
