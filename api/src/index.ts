a2f460fcaa87a3c09685c67040a0333123b3f1bb
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-11-04 18:43:57 +0200

    api pagination added

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,14 +11,34 @@ app.use(cors());
 app.use(express.static('./static'));
 
 app.get('/products', (req, res) => {
+  const {limit, page} = req.query;
+
   fs.readFile('./static/phones.json', 'utf-8', (err, data) => {
     if (err != null) {
       res.sendStatus(404);
     }
-    res.send(JSON.parse(data));
+    const parsedData = JSON.parse(data);
+
+    const validLimit
+    = limit 
+      ? Math.min(+limit, parsedData.length)
+      : parsedData.length;
+    
+    const validPage
+      = page 
+        ? Math.min(+page, Math.ceil(parsedData.length / validLimit))
+        : Math.ceil(parsedData.length / validLimit);
+
+    const start = (validPage - 1) * validLimit; 
+    const end = Math.max(validPage * validLimit, parsedData.length); 
+    const validData = parsedData.slice(start, end);
+
+    res.append('X-Total-Count', String(parsedData.length));
+    res.append('Access-Control-Expose-Headers', 'X-Total-Count');
+    res.send(validData);
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server was started on: ${PORT}`);
+  console.log(`Server was started on ${PORT}`);
 });
--- fe/src/api/apiService.js
+++ fe/src/api/apiService.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
-const URL = 'https://syndicate-product-catalog.herokuapp.com';
+// const URL = 'https://syndicate-product-catalog.herokuapp.com';
+const URL = 'http://localhost:5000';
+
+// useEffect(() => {
+//   const getPhonesAsync = async () => {
+//     const receivedTodos = await getPhones(8, 4);
+//     console.log('receivedTodos: ', receivedTodos);
+//   };
+//   getPhonesAsync();
+// }, []);
 
 export const getPhones = async (limit = 10, page = 1) => {
   const phonesResponse = await axios.get(`${URL}/products`, {
