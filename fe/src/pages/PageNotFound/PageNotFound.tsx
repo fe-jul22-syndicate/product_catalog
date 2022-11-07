0d30f5b7e9ca9063239f3b9f02535b4d562b46cc
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-11-07 08:49:10 +0200

    router added

---
+++ fe/src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const PageNotFound: React.FC = () => (
+  <section className="section">
+    <div className="container">
+      <h1 className="title">Page Not Found</h1>
+    </div>
+  </section>
+);
---
+++ fe/src/pages/TabletsPage/TabletsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TabletsPage: React.FC = () => (
+  <section className="section">
+    <div className="container">
+      <h1 className="title">Tablets Page</h1>
+    </div>
+  </section>
+);
